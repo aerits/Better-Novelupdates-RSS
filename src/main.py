124b62656b943c8edf22107b0d1c3c3b4f85572a
aerits <stev_nm@protonmail.com>
2023-05-29 22:04:48 -0600

    it work now

--- src/main.py
+++ src/main.py
@@ -1,23 +1,60 @@
 import requests
 import subprocess
 import os
+import sys
 
 import requests
 import subprocess
 import os
+import sys
 os.system("chmod +x ./src/scripts/downloadPage.sh")
 def getSeriesId(link):
-    seriesid = subprocess.check_output(['sh', './src/scripts/downloadPage.sh', link])
+    seriesid = subprocess.check_output(['sh', './src/scripts/downloadPage.sh', link, '0'])
     seriesid = str(seriesid).split('"')[1][:-6]
     return seriesid
 
 import requests
 import subprocess
 import os
+import sys
 os.system("chmod +x ./src/scripts/downloadPage.sh")
 def getSeriesId(link):
-    seriesid = subprocess.check_output(['sh', './src/scripts/downloadPage.sh', link])
+    seriesid = subprocess.check_output(['sh', './src/scripts/downloadPage.sh', link, '0'])
     seriesid = str(seriesid).split('"')[1][:-6]
     return seriesid
 e = getSeriesId("https://www.novelupdates.com/series/two-faced-princess/")
 print(e)
+
+import requests
+import subprocess
+import os
+import sys
+import requests
+import subprocess
+import os
+import sys
+os.system("chmod +x ./src/scripts/downloadPage.sh")
+def getSeriesId(link):
+    seriesid = subprocess.check_output(['sh', './src/scripts/downloadPage.sh', link, '0'])
+    seriesid = str(seriesid).split('"')[1][:-6]
+    return seriesid
+
+def markRead(form, series, chapter):
+  seriesid = getSeriesId(series)
+  if(form>0):
+  # get latest chapter
+    chapter = subprocess.check_output(['sh', './src/scripts/downloadPage.sh', series, '1'])
+    return 1
+  url = "https://www.novelupdates.com/readinglist_update.php?rid="+chapter+"&sid="+seriesid+"&checked=yes"
+  # response = requests.get(url)
+  # print(response.status_code)
+  # print(response.text)
+  # print(url)
+  subprocess.call(['w3m', url, '-dump'])
+
+if(sys.argv[2] == '0'):
+    markRead(1, sys.argv[1], 0)
+else:
+    chapter = sys.argv[2].split("/")
+    # print(chapter[4])
+    markRead(0, sys.argv[1], chapter[4])
--- src/scripts/downloadPage.sh
+++ src/scripts/downloadPage.sh
@@ -5,4 +5,12 @@ download_page () {
 download_page () {
     w3m -dump_source $1 -o accept_encoding='*;q=0'
 }
-download_page $1 | grep shortlink | cut -d"=" -f4
+if [ $2 -eq 0 ]
+then
+    download_page $1 | grep shortlink | cut -d"=" -f4
+elif [ $2 -eq 1 ]
+then
+     download_page $1 | grep chp-release | head -n 2 | tail -n 1 | cut -d"/" -f5
+else
+    echo "ENTER VALID COMMAND"
+fi
